44a16ba4af70eafad1e3841201eb3eb765090bb6
amruta125 <68988576+amruta125@users.noreply.github.com>
2020-09-25 15:29:00 +0530

    Update HttpTriggerCSharp1.cs

--- HttpTriggerCSharp1.cs
+++ HttpTriggerCSharp1.cs
@@ -21,6 +21,7 @@ namespace Company.Function
 
             string name = req.Query["name"];
 
+            
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
